7a9a7a53b18d165067880ebdd78e05662de7f7b3
Tomàs Forné Cappeau <tomas.forne-cappeau@epitech.eu>
2021-07-11 20:20:47 +0200

    fix: Fixed bot requirements

--- src/Constants/ActionTypeConstants.ts
+++ src/Constants/ActionTypeConstants.ts
@@ -9,7 +9,7 @@ export const FailureAppendType = "_FAILURE"
 export const ModelActionType = (modelName: string, actionType: string, isSuccess?: boolean): string => `${modelName}ActionTypes/${actionType.toUpperCase()}${isSuccess !== undefined ? (isSuccess ? SuccessAppendType : FailureAppendType) : ''}`
 
 export const ActionTypeContent = (modelName: string, actionType: string, properties: ParsedProperty[], isSuccess?: boolean): string => {
-    const actionTypeVarName = actionType.toUpperCase().replace(" ", "_") + (isSuccess !== undefined ? (isSuccess ? SuccessAppendType : FailureAppendType) : '')
+    const actionTypeVarName = actionType.toUpperCase().replace(/\s/g, "_") + (isSuccess !== undefined ? (isSuccess ? SuccessAppendType : FailureAppendType) : '')
 
     const actionTypeLine = `export const ${actionTypeVarName} = ${ModelActionType(modelName, actionType, isSuccess)}`
     const actionInterfaceLine = `export interface ${toCamelCase(actionType)}Action {\n    type: typeof ${actionTypeVarName}\n${propertiesToInterface(properties)}}\n`
